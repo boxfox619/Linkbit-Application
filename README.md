18adfb30983097ab6917a9b979290a4fc9093923
Boxfox <boxfoxsg619@gmail.com>
2018-10-27 17:24:08 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+LinkbitApplication3
---
+++ src/containers/GuideView.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Modal, View, Text, ActivityIndicator, } from 'react-native';
+
+export default class ProgressDialog extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <View>
+
+            </View>
+        );
+    }
+}
