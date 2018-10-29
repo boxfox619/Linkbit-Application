dfbe031fc0d3e489e30b5077d8f18af0b1a6f8b3
Boxfox <boxfoxsg619@gmail.com>
2018-10-29 17:00:25 +0900

    Wallet create, list fetching api

--- src/api/WalletApi.js
+++ src/api/WalletApi.js
@@ -1,13 +1,43 @@
+import {fetch} from 'react-native';
+import {HOST} from "../libs/Constraints";
+
 class WalletApi {
-    fetchWallets = () => {
+  fetchWallets = () => {
+    fetch(`${HOST}/wallet/list`, {
+      method: 'GET',
+      headers: {
+        'Authorization': ''
+      }
+    }).then(res => {
 
-    }
+    })
+    .catch(error => {
 
-    saveWallet = (wallet) => {
+    });
+  };
 
-    }
+  saveWallet = (address, name, password, description, major, open) => {
+  }
 
-    createWallet = (wallet) => {
+  createWallet = (wallet) => {
+    fetch(`${HOST}/wallet/new`, {
+      method: 'GET',
+      headers: {
+        'Authorization': ''
+      },
+      body: JSON.stringify({
+        address,
+        name,
+        password,
+        description,
+        major,
+        open
+      })
+    }).then(res => {
 
-    }
+    })
+    .catch(error => {
+
+    });
+  }
 }
