351bc21e2bd6f3bde4beb6f6295e116b37801228
Boxfox <boxfoxsg619@gmail.com>
2018-11-07 23:19:00 +0900

    Remove ant design settings

--- src/containers/MainView/MainTabView.js
+++ src/containers/MainView/MainTabView.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {View, StyleSheet, Text} from 'react-native';
+import {View, StyleSheet} from 'react-native';
 import WalletListView from "./WalletListView";
 
 export default class MainTabView extends React.Component {
