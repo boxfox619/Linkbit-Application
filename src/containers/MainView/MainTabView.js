f3f9b25497ed0aa35377ee62b060c89c483d1ff2
Boxfox <boxfoxsg619@gmail.com>
2018-11-09 22:35:49 +0900

    Implement header bar

--- src/containers/MainView/MainTabView.js
+++ src/containers/MainView/MainTabView.js
@@ -1,13 +1,21 @@
 import React from 'react';
-import {View, StyleSheet} from 'react-native';
+import {View, StyleSheet, Text} from 'react-native';
 import WalletListView from "./WalletListView";
+import {Header} from 'react-native-elements';
 
 export default class MainTabView extends React.Component {
 
     render() {
         return (
             <View style={styles.container}>
-                <WalletListView/>
+                <Header
+                    backgroundColor={'#ffffff'}
+                    leftComponent={<Text style={styles.title}>Wallet</Text>}
+                    rightComponent={{ icon: 'payment', color: '#000000' }}
+                />
+                <View style={styles.content}>
+                    <WalletListView/>
+                </View>
             </View>
         )
     }
@@ -15,6 +23,14 @@ export default class MainTabView extends React.Component {
 
 const styles = StyleSheet.create({
     container: {
+        flex: 1
+    },
+    title: {
+        fontSize: 17,
+        fontWeight: 'bold',
+        marginLeft: 10,
+    },
+    content: {
         flex: 1,
         paddingVertical: 40,
         paddingHorizontal: 20
