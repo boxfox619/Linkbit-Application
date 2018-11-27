dc320f087d98abb9c26d72788a7198800472891f
firepunch <firepunch119@gmail.com>
2018-11-28 00:47:16 +0900

    SettingView add RenderView for lang
    나중에는 각 세팅마다 렌더링

--- src/containers/SettingView/SettingView.js
+++ src/containers/SettingView/SettingView.js
@@ -32,6 +32,30 @@ const ko = {
 i18n.fallbacks = true
 i18n.translations = { en, ko }
 i18n.locale = Localization.locale
+
+const RenderView = (props => {
+  const { viewName } = props
+
+  const langList = [{
+    name: i18n.t('lang_ko', {locale: 'en'}),
+    txt: i18n.t('lang_ko'),
+  }, {
+    name: i18n.t('lang_en', {locale: 'en'}),
+    txt: i18n.t('lang_en'),
+  }]
+
+  return (
+    <View style={styles.container}>
+      {
+        langList.map((item, idx) =>
+          <TouchableOpacity key={idx} style={styles.listItem}>
+            <Text>{item.txt}</Text>
+          </TouchableOpacity>)
+      }
+    </View>
+  )
+})
+
 @observer
 export default class SettingView extends React.Component {
   @observable selectedSettingName = 'SettingList'
