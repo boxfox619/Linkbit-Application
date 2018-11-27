547e7d0c3f68fd0653c53e62ca9e266e509991c8
firepunch <firepunch119@gmail.com>
2018-11-28 00:44:44 +0900

    SettingView change property key, sub to mainTxt, subTxt

--- src/containers/SettingView/SettingView.js
+++ src/containers/SettingView/SettingView.js
@@ -1,66 +1,100 @@
 import React from 'react'
 import { StyleSheet, View, TouchableOpacity, Text } from 'react-native'
+import {observable} from 'mobx'
+import {observer} from 'mobx-react'
 import { Localization } from 'expo-localization'
 import i18n from 'i18n-js'
 const en = {
-  lang_key: 'Language',
-  lang_sub: 'You can select a language by country',
-  bill_key: 'Currency',
-  bill_sub: 'You can set the unit of currency',
-  lock_key: 'Security',
-  lock_sub: 'You can set up security methods',
-  reset_key: 'Reset',
-  reset_sub: 'We destroy all accounts and information'
+  lang_mainTxt: 'Language',
+  lang_subTxt: 'You can select a language by country',
+  lang_en: 'English',
+  lang_ko: 'Korean',
+  bill_mainTxt: 'Currency',
+  bill_subTxt: 'You can set the unit of currency',
+  lock_mainTxt: 'Security',
+  lock_subTxt: 'You can set up security methods',
+  reset_mainTxt: 'Reset',
+  reset_subTxt: 'We destroy all accounts and information'
 }
 const ko = {
-  lang_key: '언어 설정',
-  lang_sub: '나라별 언어를 선택할 수 있습니다',
-  bill_key: '화폐 단위',
-  bill_sub: '화폐 단위를 설정할 수 있습니다',
-  lock_key: '보안 설정',
-  lock_sub: '보안 방법을 설정할 수 있습니다',
-  reset_key: '초기화',
-  reset_sub: '모든 계정과 정보를 파기합니다',
+  lang_mainTxt: '언어 설정',
+  lang_subTxt: '나라별 언어를 선택할 수 있습니다',
+  lang_en: '영어',
+  lang_ko: '한국어',
+  bill_mainTxt: '화폐 단위',
+  bill_subTxt: '화폐 단위를 설정할 수 있습니다',
+  lock_mainTxt: '보안 설정',
+  lock_subTxt: '보안 방법을 설정할 수 있습니다',
+  reset_mainTxt: '초기화',
+  reset_subTxt: '모든 계정과 정보를 파기합니다',
 }
 
-i18n.fallbacks = true;
-i18n.translations = { en, ko };
-i18n.locale = Localization.locale;
-export default class SettingView extends React.Component {
-  render () {
-    const list = [
+i18n.fallbacks = true
+i18n.translations = { en, ko }
+i18n.locale = Localization.locale
+
+const RenderView = (props => {
+  const { viewName } = props
+
+  const langList = [{
+    name: i18n.t('lang_ko', {locale: 'en'}),
+    txt: i18n.t('lang_ko'),
+  }, {
+    name: i18n.t('lang_en', {locale: 'en'}),
+    txt: i18n.t('lang_en'),
+  }]
+
+  return (
+    <View style={styles.container}>
       {
-        key: i18n.t('lang_key'),
-        sub: i18n.t('lang_sub'),
-      }, {
-        key: '화폐 단위',
-        sub: '화폐 단위를 설정할 수 있습니다',
-      }, {
-        key: '보안 설정',
-        sub: '계정 정보를 선택할 수 있습니다',
-      }, {
-        key: '1:1 문의',
-        sub: '운영진에게 문의할 수 있습니다',
-      }, {
-        key: '신고하기',
-        sub: '앱 내에서 문제가 발생할 경우 신고하실 수 있습니다',
-      }, {
-        key: '초기화',
-        sub: '모든 계정과 정보를 파기합니다',
-      }]
+        langList.map((item, idx) =>
+          <TouchableOpacity key={idx} style={styles.listItem}>
+            <Text>{item.txt}</Text>
+          </TouchableOpacity>)
+      }
+    </View>
+  )
+})
+
+@observer
+export default class SettingView extends React.Component {
+  @observable selectedSettingName = 'SettingList'
+
+  render () {
+    const settingList = [{
+      name: i18n.t('lang_mainTxt', {locale: 'en'}),
+      mainTxt: i18n.t('lang_mainTxt'),
+      subTxt: i18n.t('lang_subTxt'),
+    }, {
+      name: i18n.t('bill_mainTxt', {locale: 'en'}),
+      mainTxt: i18n.t('bill_mainTxt'),
+      subTxt: i18n.t('bill_subTxt'),
+    }, {
+      name: i18n.t('lock_mainTxt', {locale: 'en'}),
+      mainTxt: i18n.t('lock_mainTxt'),
+      subTxt: i18n.t('lock_subTxt'),
+    }, {
+      name: i18n.t('reset_mainTxt', {locale: 'en'}),
+      mainTxt: i18n.t('reset_mainTxt'),
+      subTxt: i18n.t('reset_subTxt'),
+    }]
 
     return (
       <View style={styles.container}>
         {
-          list.map((item, idx)=>
-            <TouchableOpacity key={idx} onPress={this.onPress} style={styles.listItem}>
-              <Text style={[styles.key, item.key === '초기화' && styles.reset]}>{item.key}</Text>
-              <Text style={styles.sub}>{item.sub}</Text>
-            </TouchableOpacity>)
+          viewName === 'SettingList' ?
+          settingList.map((item, idx) =>
+            <TouchableOpacity key={idx} onPress={() => this.onPressSetting(item.name)} style={styles.listItem}>
+              <Text style={[styles.mainTxt, item.mainTxt === '초기화' && styles.reset]}>{item.mainTxt}</Text>
+              <Text style={styles.subTxt}>{item.subTxt}</Text>
+            </TouchableOpacity>) :
+            <RenderView viewName={viewName}/>
         }
       </View>
     )
   }
+
+  onPressSetting = name => this.selectedSettingName = name
 }
 
 const styles = StyleSheet.create({
@@ -78,10 +112,10 @@ const styles = StyleSheet.create({
   reset: {
     color: '#ff6767',
   },
-  key: {
+  mainTxt: {
     fontSize: 14,
   },
-  sub: {
+  subTxt: {
     fontSize: 10,
     color: '#808080',
     textAlign: 'right',
