7e573f401e60d3a58eaad111756844265422789c
firepunch <firepunch119@gmail.com>
2018-12-04 23:11:11 +0900

    Reimport SecurityView instead of PinCodeView

--- src/containers/index.js
+++ src/containers/index.js
@@ -1,8 +1,8 @@
 import SettingView from './SettingView/SettingView';
 import FingerPrintView from './SecurityView/FingerPrintView';
 import GuideView from './GuideView/GuideView';
-import PinCodeInputView from './SecurityView/PinCodeInputView';
+import SecurityView from './SecurityView/SecurityView';
 import TermsOfServiceView from './TermsOfServiceView';
 import MainTabView from "./MainView/MainTabView";
 
-export {SettingView, FingerPrintView, GuideView, PinCodeInputView, TermsOfServiceView, MainTabView};
+export {SettingView, FingerPrintView, GuideView, SecurityView, TermsOfServiceView, MainTabView};
