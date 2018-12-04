041d923f1e4bb6106cdb615d59778ad6b738fe89
firepunch <firepunch119@gmail.com>
2018-12-04 23:09:44 +0900

    Rename PinCodeView => PinCodeInput, PinCodeInputView => PinCodeView
    PinCodeView separated function into verify and change

--- src/components/index.js
+++ src/components/index.js
@@ -1,6 +1,6 @@
 import ProgressDialog from './ProgressDialog';
 import {PagerDotIndicator, IndicatorViewPager} from './ViewPager';
-import PinCodeView from './PinCodeView';
+import PinCodeView from './PinCodeInput';
 import CoinCard from './WalletCard/CoinCard';
 import WalletCard from './WalletCard/WalletCard';
 import WalletList from './WalletList/WalletList';
