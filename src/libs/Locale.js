1a16f42db284ead9d419820e7c6b7678f44b4181
firepunch <firepunch119@gmail.com>
2018-12-09 13:09:48 +0900

    move locale content SettingView to Locale file

---
+++ src/libs/Locale.js
@@ -0,0 +1,38 @@
+import { Localization } from 'expo-localization'
+import i18n from 'i18n-js'
+
+const en = {
+  lang_mainTxt: 'Language',
+  lang_subTxt: 'You can select a language by country',
+  lang_en: 'English',
+  lang_ko: 'Korean',
+  bill_mainTxt: 'Currency',
+  bill_subTxt: 'You can set the unit of currency',
+  bill_usd: 'USD - US Dollar',
+  bill_krw: 'KRW - Korea Won',
+  lock_mainTxt: 'Security',
+  lock_subTxt: 'You can set up security methods',
+  reset_mainTxt: 'Reset',
+  reset_subTxt: 'We destroy all accounts and information',
+}
+const ko = {
+  lang_mainTxt: '언어 설정',
+  lang_subTxt: '나라별 언어를 선택할 수 있습니다',
+  lang_en: '영어',
+  lang_ko: '한국어',
+  bill_mainTxt: '화폐 단위',
+  bill_subTxt: '화폐 단위를 설정할 수 있습니다',
+  bill_en: '화폐 단위를 설정할 수 있습니다',
+  bill_usd: 'USD - 미국 달러',
+  bill_krw: 'KRW - 한국 원',
+  lock_mainTxt: '보안 설정',
+  lock_subTxt: '보안 방법을 설정할 수 있습니다',
+  reset_mainTxt: '초기화',
+  reset_subTxt: '모든 계정과 정보를 파기합니다',
+}
+
+i18n.fallbacks = true
+i18n.translations = { en, ko }
+i18n.locale = Localization.locale
+
+export default i18n
