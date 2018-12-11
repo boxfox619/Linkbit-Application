4ed9830e086b41f9bed9ab199401aae0786fbee1
Boxfox <boxfoxsg619@gmail.com>
2018-12-11 18:40:31 +0900

    Implement address network api & prepare for auth api

--- src/api/Address/AddressNetworkApi.js
+++ src/api/Address/AddressNetworkApi.js
@@ -3,23 +3,56 @@ import {fetch} from "react-native";
 
 export default class AddressNetworkApi {
 
-    fetchOwnAddressList = async () => {
-
+    fetchOwnAddressList = async (uid) => {
+        try {
+            const res = await fetch(`${HOST}/address`, {
+                method: 'GET',
+                headers: { 'Authorization': uid }
+            });
+            return res.json();
+        } catch (error) {
+            console.log(error);
+        }
     };
 
-    registerAddress = async (linkedAddress, accountAddress) => {
+    registerAddress = async (linkAddress, symbol, accountAddress) => {
         try {
-            const res = await fetch(`${HOST}/address/link`, {
+            const res = await fetch(`${HOST}/address`, {
                 method: 'POST',
-                headers: {
-                    'Authorization': ''
-                },
+                headers: { 'Authorization': '' },
                 body: JSON.stringify({
-                    linkedAddress: linkedAddress,
-                    symbol: accountAddress.symbol,
-                    accountAddress: accountAddress.address
+                    linkAddress: linkAddress,
+                    symbol: symbol,
+                    accountAddress: accountAddress
                 })
             });
+            let resJson = res.json();
+            return resJson;
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+    unregisterAddress = async (linkAddress, symbol) => {
+        try {
+            const res = await fetch(`${HOST}/address`, {
+                method: 'DELETE',
+                headers: { 'Authorization': '' },
+                body: JSON.stringify({
+                    linkAddress: linkAddress,
+                    symbol: symbol
+                })
+            });
+            let resJson = res.json();
+            return resJson;
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+    checkLinkAddressExists = async (linkAddress) => {
+        try {
+            const res = await fetch(`${HOST}/address/valid?address=${linkAddress}`, {method: 'GET'});
             let resJson = res.json();
             return resJson;
         } catch (error) {
---
+++ src/api/Auth/AuthApiMock.js
@@ -0,0 +1,7 @@
+export default class AuthApiMock {
+
+    loadPrivateKey = async () => {
+        return {};
+    };
+
+}
