12611ed51bc4826c35046824ea6b97b6c79d8741
Boxfox <boxfoxsg619@gmail.com>
2018-12-20 23:49:59 +0900

    Implement wallet create function

--- src/containers/CreateWalletView/InputWalletInformationView/InputWalletInformationView.js
+++ src/containers/CreateWalletView/InputWalletInformationView/InputWalletInformationView.js
@@ -4,14 +4,17 @@ import NavigationButton from '../../../components/NavigationButton/NavigationBut
 import Input from '../../../components/Input/Input'
 import SegmentedControl from '../../../components/SegmentedControl/SegmentedControl'
 import withTitle from '../../../components/HOC/withTitle'
-import BorderCard from "../../../components/Card/BorderCard";
 import Coin from "../Coin/Coin";
+import { inject, observer } from 'mobx-react'
 
 const InputWithTitle = withTitle(Input)
 const SegmentedControlWithTitle = withTitle(SegmentedControl)
 
+@inject(['wallet'])
+@observer
 export default class InputWalletInformationView extends React.Component {
     state = {
+        progress: false,
         selectedIndex: 0,
         coin: {}
     }
@@ -30,21 +33,34 @@ export default class InputWalletInformationView extends React.Component {
                     <View style={{ paddingHorizontal: 20 }}>
                         <Coin name={coin.name} symbol={coin.symbol} themeColor={coin.themeColor} onPress={()=>{}} activate={true}/>
                         <InputWithTitle title={'지갑 이름'} />
-                        <InputWithTitle title={'지갑 설명'} />
+                        {/*<InputWithTitle title={'지갑 설명'} />*/}
                         <InputWithTitle title={'비밀번호'}
-                            secureTextEntry={true} />
+                                        secureTextEntry={true} />
                         <InputWithTitle title={'비밀번호 재입력'}
-                            secureTextEntry={true} />
-                        <SegmentedControlWithTitle title={'공개범위 설정'}
-                            options={['소유자 정보', '지갑 정보']}
-                            selectedIndex={selectedIndex}
-                            onChange={index => this.setState({ selectedIndex: index })} />
+                                        secureTextEntry={true} />
+                        {/*<SegmentedControlWithTitle title={'공개범위 설정'}
+                                                   options={['소유자 정보', '지갑 정보']}
+                                                   selectedIndex={selectedIndex}
+                                                   onChange={index => this.setState({ selectedIndex: index })} />*/}
                     </View>
-                    <NavigationButton title={'생성하기'}
-                        onPress={()=>this.props.navigation.navigate('Main')} />
+                    <NavigationButton title={'생성하기'} onPress={this.createWallet} />
                 </View>
             </SafeAreaView>
         )
+    }
+
+    createWallet = () => {
+        this.setState({progress: true})
+        const {coin} = this.state
+        const name = ''
+        const password = ''
+        this.props.wallet.createWallet(coin.symbol, name, password).then(() => {
+            this.setState({progress: false})
+            this.props.navigation.navigate('Main')
+        }).catch(e => {
+            this.setState({progress: false})
+            alert(`지갑 생성 실패 ${e}`)
+        })
     }
 }
 
