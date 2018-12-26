d1a728db5b8862a04eb5ae445023b61a3c879b78
Boxfox <boxfoxsg619@gmail.com>
2018-12-27 00:52:11 +0900

    Prepare for withdraw store

---
+++ src/store/Withdraw/WithdrawStore.js
@@ -0,0 +1,37 @@
+import {observable, action, computed, runInAction} from 'mobx'
+import WalletStore from "../Wallet/WalletStore";
+import TransactionStore from "../Transaction/TransactionStore";
+
+export default class WithdrawStore {
+    @observable symbol
+    @observable sourceAddress
+    @observable destAddress
+    @observable amount
+    walletStore
+    transactionStore
+    withdrawApi
+    //@TODO Impl withdraw api
+
+    constructor() {
+        this.walletStore = WalletStore
+
+    }
+
+    @action setSoruceWallet(symbol, address) {
+        this.symbol = symbol
+        this.sourceAddress = address
+    }
+
+    @action setTargetAddress(address) {
+        this.destAddress = address
+    }
+
+    @action setAmount(amount){
+        this.amount = amount
+    }
+
+    withdraw = async (password) => {
+        const wallet = this.walletStore.walletList.filter(w => w.address === this.sourceAddress || w.linkedAddress === this.sourceAddress)
+        this.transactionStore = new TransactionStore(this.symbol, this.sourceAddress)
+    }
+}
