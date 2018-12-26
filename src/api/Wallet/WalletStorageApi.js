5c489509fed6e30680abe1e67f6341c67a78c592
Boxfox <boxfoxsg619@gmail.com>
2018-12-26 22:22:52 +0900

    Implement wallet create api

--- src/api/Wallet/WalletStorageApi.js
+++ src/api/Wallet/WalletStorageApi.js
@@ -1,6 +1,25 @@
-import {AsyncStorage} from 'react-native';
+import {AsyncStorage, fetch} from 'react-native';
+import {HOST} from "../../libs/Constraints";
 
 export default class WalletStorageApi {
+
+    createWallet = async (symbol, password) => {
+        try {
+            const res = await await fetch(`${HOST}/wallet`, {
+                method: 'POST',
+                headers: {
+                    'Authorization': '',
+                },
+                body: JSON.stringify({
+                    symbol,
+                    password
+                }),
+            });
+            return res;
+        } catch (error) {
+            console.log(error);
+        }
+    }
 
     loadWallets = async () => {
         try {
