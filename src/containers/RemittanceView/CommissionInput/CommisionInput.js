6d80f7fb3d668114b50e5e8514a08d39818a2643
Ashton Yoon <y109384@naver.com>
2019-01-19 01:43:21 +0900

    Add commission view

---
+++ src/containers/RemittanceView/CommissionInput/CommisionInput.js
@@ -0,0 +1,71 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import { View, StyleSheet, Slider, Text } from 'react-native'
+import { PRIMARY_COLOR } from '../../../libs/Constraints'
+
+export default class CommissionInput extends React.Component {
+    static propTypes = {
+        commission: PropTypes.number.isRequired,
+        onValueChange: PropTypes.func.isRequired
+    }
+
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            commission: props.commission,
+            onValueChange: props.onValueChange
+        }
+    }
+
+    render() {
+        const { commission, onValueChange } = this.state
+
+        return (
+            <View style={styles.container}>
+                <Slider step={1}
+                    minimumValue={0}
+                    maximumValue={4}
+                    maximumTrackTintColor={'#EBEBEB'}
+                    minimumTrackTintColor={PRIMARY_COLOR}
+                    onValueChange={onValueChange}
+                    value={commission} />
+                <View style={styles.gridContainer}>
+                    <View style={styles.line} />
+                    <View style={styles.line} />
+                    <View style={styles.line} />
+                    <View style={styles.line} />
+                    <View style={styles.line} />
+                </View>
+                <Text style={styles.description}>{"수수료 금액에 따라서 전송 속도가 결정됩니다"}</Text>
+            </View>
+        )
+    }
+}
+
+const styles = StyleSheet.create({
+    container: {
+        width: '100%'
+    },
+    gridContainer: {
+        width: '100%',
+        display: 'flex',
+        flexDirection: 'row',
+        paddingHorizontal: 14,
+        justifyContent: 'space-between'
+    },
+    line: {
+        width: 2,
+        height: 10,
+        borderRadius: 1,
+        backgroundColor: 'gray'
+    },
+    description:{
+        width: '100%',
+        fontSize: 20,
+        fontWeight: 'bold',
+        color: '#A7A8AC',
+        textAlign: 'center',
+        paddingVertical: 40,
+    }
+})
--- src/containers/RemittanceView/RemittanceView.js
+++ src/containers/RemittanceView/RemittanceView.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import {View, StyleSheet, Text} from 'react-native'
+import { View, StyleSheet, Text } from 'react-native'
 import NavigationButton from '../../components/NavigationButton/NavigationButton'
 import AddressBox from './AddressBox/AddressBox'
 import AddressInput from './AddressInput/AddressInput'
@@ -7,8 +7,9 @@ import AmountInput from './AmountInput/AmountInput'
 import AmountBox from './AmountBox/AmountBox'
 import RemittanceType from '../../store/RemittanceType'
 import WalletSummaryCard from "../../components/Card/WalletSummaryCard"
-import {inject, observer} from "mobx-react/index"
-import {observable} from 'mobx'
+import CommissionInput from "./CommissionInput/CommisionInput"
+import { inject, observer } from "mobx-react/index"
+import { observable } from 'mobx'
 import WithdrawStore from '../../store/Withdraw/WithdrawStore'
 
 @inject(['coin'])
@@ -17,6 +18,8 @@ export default class RemmittanceView extends React.Component {
     @observable step = 1
     @observable method = RemittanceType.Wallet
     @observable calculateSymbol = 'USD'
+    @observable commission = 0
+    @observable ratio = 0.1582
 
     constructor(props) {
         super(props)
@@ -28,22 +31,25 @@ export default class RemmittanceView extends React.Component {
 
     nextStep = () => {
         if (this.method === RemittanceType.Wallet) {
-            switch (this.step){
+            switch (this.step) {
                 case 1:
-                    if(this.withdrawStore.destAddressError){
+                    if (this.withdrawStore.destAddressError) {
                         alert(this.withdrawStore.destAddressError)
                         break;
                     }
-                    this.step +=1
+                    this.step += 1
                     break;
                 case 2:
-                    if(this.withdrawStore.amountError){
+                    if (this.withdrawStore.amountError) {
                         alert(this.withdrawStore.amountError)
                         break;
                     }
-                    this.step +=1
+                    this.step += 1
                     break;
                 case 3:
+                    this.step += 1
+                    break;
+                case 4:
                     this.withdrawStore.withdraw('1234')
                     break;
             }
@@ -51,20 +57,20 @@ export default class RemmittanceView extends React.Component {
     }
 
     render() {
-        const {method, calculateSymbol, step} = this
-        const {destAddress, amount, price, symbol, moneySymbol} = this.withdrawStore
+        const { method, calculateSymbol, step, commission, ratio } = this
+        const { destAddress, amount, price, symbol, moneySymbol } = this.withdrawStore
         const wallet = this.wallet
         return (
             <View style={styles.container}>
                 <View style={styles.wrapper}>
                     <Text style={styles.title}>출금 지갑</Text>
-                    <WalletSummaryCard wallet={wallet}/>
+                    <WalletSummaryCard wallet={wallet} />
                     {step < 2 && method === RemittanceType.Wallet && (
                         <React.Fragment>
                             <Text style={styles.title}>받는 주소</Text>
                             <AddressInput
                                 address={destAddress}
-                                onChangeText={destAddress => this.withdrawStore.setTargetAddress(destAddress)}/>
+                                onChangeText={destAddress => this.withdrawStore.setTargetAddress(destAddress)} />
                         </React.Fragment>
                     )
                     }
@@ -72,7 +78,7 @@ export default class RemmittanceView extends React.Component {
                         step >= 2 && method === RemittanceType.Wallet && (
                             <React.Fragment>
                                 <Text style={styles.title}>받는 주소</Text>
-                                <AddressBox address={destAddress}/>
+                                <AddressBox address={destAddress} />
                             </React.Fragment>
                         )
                     }
@@ -86,25 +92,36 @@ export default class RemmittanceView extends React.Component {
                             symbols={[[wallet.symbol, moneySymbol]]}
                             amount={(calculateSymbol === symbol) ? amount : price}
                             onChangeAmount={this.handleChangeAmount}
-                            onChangeSymbol={(item) => this.calculateSymbol = item}/>
+                            onChangeSymbol={(item) => this.calculateSymbol = item} />
                     }
                     {
-                        step === 3 &&
+                        step >= 3 &&
                         <AmountBox price={price}
-                                   moneySymbol={moneySymbol}
-                                   symbol={symbol}
-                                   amount={amount}/>
+                            moneySymbol={moneySymbol}
+                            symbol={symbol}
+                            amount={amount} />
+                    }
+                    {
+                        step === 4 &&
+                        <React.Fragment>
+                            <View style={styles.commissionHeader}>
+                                <Text style={styles.title}>수수료</Text>
+                                <Text style={[styles.title, { fontSize: 18 }]}>{`${symbol} ${commission * ratio}`}</Text>
+                            </View>
+                            <CommissionInput commission={commission}
+                                onValueChange={this.onCommissionChanged} />
+                        </React.Fragment>
                     }
                 </View>
                 <NavigationButton
-                    title={step === 3 ? '송금하기' : '다음'}
-                    onPress={this.nextStep}/>
+                    title={step === 4 ? '송금하기' : '다음'}
+                    onPress={this.nextStep} />
             </View>
         )
     }
 
     handleChangeAmount = (amount) => {
-        const {symbol} = this.withdrawStore
+        const { symbol } = this.withdrawStore
         if (this.calculateSymbol === symbol) {
             this.withdrawStore.setAmount(amount)
         } else {
@@ -114,6 +131,10 @@ export default class RemmittanceView extends React.Component {
 
     get wallet() {
         return this.props.navigation.getParam("wallet")
+    }
+
+    onCommissionChanged = (value) => {
+        this.commission = value
     }
 }
 
@@ -137,9 +158,13 @@ const styles = StyleSheet.create({
         color: '#594343',
         fontSize: 14,
         fontWeight: 'bold',
-        width: '100%',
         marginHorizontal: 'auto',
         marginTop: 20,
         marginBottom: 5,
     },
+    commissionHeader: {
+        display: 'flex',
+        flexDirection: 'row',
+        justifyContent: 'space-between'
+    }
 })
