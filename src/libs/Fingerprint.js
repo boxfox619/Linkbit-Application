7305eee90f6474253be9c9693ba0c610b54e9316
firepunch <firepunch119@gmail.com>
2019-02-03 11:45:13 +0900

    create libs/Fingerprint proccess

---
+++ src/libs/Fingerprint.js
@@ -0,0 +1,36 @@
+import {Platform} from 'react-native'
+
+export const checkForFingerprints = async isSetting => {
+  let records = await Expo.LocalAuthentication.isEnrolledAsync()
+
+  if (isSetting) {
+    return records
+  } else if (records) {
+    return this.handleLoginPress()
+  } else {
+    alert('Please ensure you have set up biometrics in your OS settings.')
+  }
+}
+
+export const handleLoginPress = () => {
+  if (Platform.OS === 'android') {
+    this.showAndroidAlert()
+  } else {
+    this.scanFingerprint()
+  }
+}
+
+export const showAndroidAlert = () => {
+  Alert.alert('Fingerprint Scan', 'Place your finger over the touch sensor.')
+  this.scanFingerprint()
+}
+
+export const scanFingerprint = async () => {
+  let result = await Expo.LocalAuthentication.authenticateAsync('Biometric Scan.')
+  if (!result.success) {
+    //TODO Try Again 뜨는지 확인
+    //TODO Enter Password 작동확인
+    // Cancel
+    alert('error')
+  }
+}
