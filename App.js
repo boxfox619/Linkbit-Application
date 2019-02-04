060e27a2593252a431c44406e9919ac2c3b504a2
firepunch <firepunch119@gmail.com>
2019-02-05 02:02:25 +0900

    App chk app is locked

--- App.js
+++ App.js
@@ -3,8 +3,11 @@ import {StyleSheet, View} from 'react-native'
 import {createAppContainer} from 'react-navigation'
 import {Provider} from 'mobx-react'
 import Navigator from './src/containers/navigator'
+import PinCodeView  from './src/components/PinCodeInput'
 import {WalletStore, CoinPriceStore, AddressStore, SettingStore} from './src/store'
 import {checkForFingerprints} from './src/libs/Fingerprint'
+import {observer} from 'mobx-react/index'
+import {observable} from 'mobx/lib/mobx'
 
 const store = {
     wallet: WalletStore,
@@ -14,23 +17,47 @@ const store = {
 }
 const AppContainer = createAppContainer(Navigator)
 
+@observer
 export default class App extends React.Component {
-    componentDidMount() {
-      const {pin, useFingerprint} = store.setting
+  @observable label = 'PIN 번호를 입력해주세요'
+  @observable isVerify = true
 
+    componentDidMount() {
       store.address.loadAddressList()
       store.wallet.loadWalletList()
       store.coin.loadCoins()
 
-      //TODO if use fingerprint or pin
-      useFingerprint && checkForFingerprints()
+      const saveFingerPrint = store.setting.getFingerprint()
+      const savePin = store.setting.getPin()
+      if(saveFingerPrint) {
+        this.isVerify = checkForFingerprints()
+      } else if(savePin) {
+        this.isVerify = false
+      }
     }
 
+  onPinVerify =  pin => {
+    const savePin =  store.setting.getPin()
+    //TODO 왜 savePin 안 나올까요..?
+    if (savePin === pin) {
+      this.isVerify = true
+    } else {
+      this.label = 'PIN 번호가 일치하지 않습니다'
+    }
+  }
+
     render() {
-        return (
+      return (
             <Provider {...store}>
                 <View style={styles.container}>
-                    <AppContainer/>
+                  {
+                    !this.isVerify ?
+                      <PinCodeView
+                        label={this.label}
+                        onComplete={(val, clear) => this.onPinVerify(val, clear())}
+                        pinLength={5}/> :
+                      <AppContainer/>
+                  }
                 </View>
             </Provider>
         )
