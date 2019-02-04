0eb8949e8ecf9e753cc2f9a566033d31a13d018c
firepunch <firepunch119@gmail.com>
2019-02-05 02:02:47 +0900

    lint indent

--- App.js
+++ App.js
@@ -3,17 +3,17 @@ import {StyleSheet, View} from 'react-native'
 import {createAppContainer} from 'react-navigation'
 import {Provider} from 'mobx-react'
 import Navigator from './src/containers/navigator'
-import PinCodeView  from './src/components/PinCodeInput'
+import PinCodeView from './src/components/PinCodeInput'
 import {WalletStore, CoinPriceStore, AddressStore, SettingStore} from './src/store'
 import {checkForFingerprints} from './src/libs/Fingerprint'
 import {observer} from 'mobx-react/index'
 import {observable} from 'mobx/lib/mobx'
 
 const store = {
-    wallet: WalletStore,
-    coin: CoinPriceStore,
-    address: AddressStore,
-    setting: SettingStore
+  wallet: WalletStore,
+  coin: CoinPriceStore,
+  address: AddressStore,
+  setting: SettingStore
 }
 const AppContainer = createAppContainer(Navigator)
 
@@ -22,22 +22,22 @@ export default class App extends React.Component {
   @observable label = 'PIN 번호를 입력해주세요'
   @observable isVerify = true
 
-    componentDidMount() {
-      store.address.loadAddressList()
-      store.wallet.loadWalletList()
-      store.coin.loadCoins()
+  componentDidMount() {
+    store.address.loadAddressList()
+    store.wallet.loadWalletList()
+    store.coin.loadCoins()
 
-      const saveFingerPrint = store.setting.getFingerprint()
-      const savePin = store.setting.getPin()
-      if(saveFingerPrint) {
-        this.isVerify = checkForFingerprints()
-      } else if(savePin) {
-        this.isVerify = false
-      }
+    const saveFingerPrint = store.setting.getFingerprint()
+    const savePin = store.setting.getPin()
+    if (saveFingerPrint) {
+      this.isVerify = checkForFingerprints()
+    } else if (savePin) {
+      this.isVerify = false
     }
+  }
 
-  onPinVerify =  pin => {
-    const savePin =  store.setting.getPin()
+  onPinVerify = pin => {
+    const savePin = store.setting.getPin()
     //TODO 왜 savePin 안 나올까요..?
     if (savePin === pin) {
       this.isVerify = true
@@ -46,27 +46,27 @@ export default class App extends React.Component {
     }
   }
 
-    render() {
-      return (
-            <Provider {...store}>
-                <View style={styles.container}>
-                  {
-                    !this.isVerify ?
-                      <PinCodeView
-                        label={this.label}
-                        onComplete={(val, clear) => this.onPinVerify(val, clear())}
-                        pinLength={5}/> :
-                      <AppContainer/>
-                  }
-                </View>
-            </Provider>
-        )
-    }
+  render() {
+    return (
+      <Provider {...store}>
+        <View style={styles.container}>
+          {
+            !this.isVerify ?
+              <PinCodeView
+                label={this.label}
+                onComplete={(val, clear) => this.onPinVerify(val, clear())}
+                pinLength={5}/> :
+              <AppContainer/>
+          }
+        </View>
+      </Provider>
+    )
+  }
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#fff',
-    },
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+  },
 })
--- src/containers/SecurityView/SecurityView.js
+++ src/containers/SecurityView/SecurityView.js
@@ -1,92 +1,92 @@
 import React from 'react'
 import {View, StyleSheet} from 'react-native'
-import PinCodeCreateView from "../PinCodeInputView/PinCodeCreateView"
-import {inject, observer} from "mobx-react"
+import PinCodeCreateView from '../PinCodeInputView/PinCodeCreateView'
+import {inject, observer} from 'mobx-react'
 import {observable} from 'mobx'
 import {checkForFingerprints} from '../../libs/Fingerprint'
-import SettingListView from "../SettingView/SettingListView"
-import PinCodeView from "../../components/PinCodeInput"
+import SettingListView from '../SettingView/SettingListView'
+import PinCodeView from '../../components/PinCodeInput'
 
 @inject(['setting'])
 @observer
 export default class SecurityView extends React.Component {
-    @observable label = '설정을 위해 PIN 번호를 입력해주세요'
-    @observable view = 'verify'
+  @observable label = '설정을 위해 PIN 번호를 입력해주세요'
+  @observable view = 'verify'
 
-    componentDidMount() {
-        const pin = this.props.setting.pin
-        this.view = !!pin ? 'verify' : 'menu'
-    }
+  componentDidMount() {
+    const pin = this.props.setting.pin
+    this.view = !!pin ? 'verify' : 'menu'
+  }
 
-    handleSetNewPin = async pin => {
-        await this.props.setting.setPin(pin)
-        this.view = 'menu'
-    handleViewSetting = view =>{
-      this.view = view
-      if(view === 'finger') {
-        this.handleSetFingerprint()
-      }
+  handleViewSetting = view => {
+    this.view = view
+    if (view === 'finger') {
+      this.handleSetFingerprint()
     }
-    }
-    onPinVerify = (pin) => {
+  }
+  handleSetNewPin = async pin => {
+    await this.props.setting.setPin(pin)
+    this.view = 'menu'
+  }
   handleSetFingerprint = async () => {
-      const {getFingerprint, setFingerprint}=this.props.settings
-      if (await getFingerprint) {
-        return this.handleOffFingerprint()
-      }
-      const finger = await checkForFingerprints(true)
+    const {getFingerprint, setFingerprint} = this.props.settings
+    if (await getFingerprint) {
+      return this.handleOffFingerprint()
+    }
+    const finger = await checkForFingerprints(true)
     await setFingerprint(finger)
     this.view = await 'menu'
   }
   handleOffFingerprint = async () => {
     return this.props.setting.setFingerprint(false)
   }
-        if (this.props.setting.pin === pin) {
-            this.view = 'menu'
-        } else {
-            this.label = 'PIN 번호가 일치하지 않습니다'
-        }
+  onPinVerify = (pin) => {
+    if (this.props.setting.pin === pin) {
+      this.view = 'menu'
+    } else {
+      this.label = 'PIN 번호가 일치하지 않습니다'
     }
+  }
 
-    render() {
-        const view = this.view
+  render() {
+    const view = this.view
 
-        return (
-            <View style={styles.container}>
-                {view === 'pin' && (<PinCodeCreateView onPinEntered={this.handleSetNewPin}/>)}
-                {(view === 'menu' || view === 'finger') && (
-                    <SettingListView list={this.settings}
-                                     style={{padding: 20}}
-                                     onItemSelected={this.handleViewSetting}/>
-                )}
-                {view === 'verify' && (
-                    <PinCodeView
-                        label={this.label}
-                        onComplete={(val, clear) => this.onPinVerify(val, clear())}
-                        pinLength={5}/>
-                )}
-            </View>
-        )
-    }
+    return (
+      <View style={styles.container}>
+        {view === 'pin' && (<PinCodeCreateView onPinEntered={this.handleSetNewPin}/>)}
+        {(view === 'menu' || view === 'finger') && (
+          <SettingListView list={this.settings}
+                           style={{padding: 20}}
+                           onItemSelected={this.handleViewSetting}/>
+        )}
+        {view === 'verify' && (
+          <PinCodeView
+            label={this.label}
+            onComplete={(val, clear) => this.onPinVerify(val, clear())}
+            pinLength={5}/>
+        )}
+      </View>
+    )
+  }
 
-    get settings() {
-        const {pin, useFingerprint} = this.props.setting
-        return [
-            {
-                labelText: '핀 코드 변경',
-                subLabelText: !!pin ? '설정됨' : '설정되지 않음',
-                key: 'pin',
-            }, {
-                labelText: '지문 변경',
-                subLabelText: useFingerprint ? '사용중' : '사용하지 않음',
-                key: 'finger',
-            }
-        ]
-    }
+  get settings() {
+    const {pin, useFingerprint} = this.props.setting
+    return [
+      {
+        labelText: '핀 코드 변경',
+        subLabelText: !!pin ? '설정됨' : '설정되지 않음',
+        key: 'pin',
+      }, {
+        labelText: '지문 변경',
+        subLabelText: useFingerprint ? '사용중' : '사용하지 않음',
+        key: 'finger',
+      }
+    ]
+  }
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1
-    }
+  container: {
+    flex: 1
+  }
 })
