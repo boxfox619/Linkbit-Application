0e913fede3ea661c00639439c297d43fc082464f
Ashton Yoon <y109384@naver.com>
2019-02-16 23:52:40 +0900

    update searchbar ui

--- src/containers/AddressView/WalletSearchView.js
+++ src/containers/AddressView/WalletSearchView.js
@@ -1,6 +1,7 @@
 import React from 'react'
-import { View, StyleSheet, FlatList } from 'react-native'
-import { SearchBar } from 'react-native-elements'
+import { View, StyleSheet, FlatList, Text } from 'react-native'
+// import { SearchBar } from 'react-native-elements'
+import SearchBar from '../../components/SearchBar/SearchBar'
 import { observer, inject } from 'mobx-react'
 import { observable } from 'mobx'
 import WalletCard from '../../components/Card/WalletCard'
@@ -10,15 +11,19 @@ import WalletCard from '../../components/Card/WalletCard'
 export default class WalletSearchView extends React.Component {
   @observable wallets
 
-  componentDidMount () {
+  componentDidMount() {
     this.onChangeText()
   }
 
   onChangeText = (text) => {
-    const {params} = this.props.navigation.state
+    const { params } = this.props.navigation.state
     const excludeAddressList = params.excludeAddressList || []
     const excludeSymbolList = params.excludeSymbolList || []
     let resultWallets = this.props.wallet.wallets
+
+    // check wallets is empty
+    if (!resultWallets) return
+
     if (!!text && text.length > 0) {
       text = text.toLowerCase()
       resultWallets = resultWallets.filter(w => (
@@ -31,33 +36,32 @@ export default class WalletSearchView extends React.Component {
   }
 
   onWalletSelected = (wallet) => {
-      const {onWalletSelected} = this.props.navigation.state.params
-      onWalletSelected(wallet)
-      this.props.navigation.goBack(null)
+    const { onWalletSelected } = this.props.navigation.state.params
+    onWalletSelected(wallet)
+    this.props.navigation.goBack(null)
   }
 
-  render () {
+  render() {
     return (
       <View style={styles.container}>
         <SearchBar
-          round
-          lightTheme
           onChangeText={this.onChangeText}
           onClearText={this.onChangeText}
-          placeholder='Wallet name or address...'/>
+          placeholder='Wallet name or address...' />
+        <Text>{'주소 목록'}</Text>
         <FlatList
           style={styles.list}
           data={this.wallets}
           keyExtractor={(item) => item.address}
-          renderItem={({item}) => {
+          renderItem={({ item }) => {
             return (
               <WalletCard
                 name={item.address}
                 symbol={item.symbol}
                 moneySymbol="KRW"
-                onPress={() => this.onWalletSelected(item)}/>
+                onPress={() => this.onWalletSelected(item)} />
             )
-          }}/>
+          }} />
       </View>
     )
   }
@@ -66,9 +70,9 @@ export default class WalletSearchView extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    paddingHorizontal: 10,
   },
   list: {
     flex: 1,
-    padding: 10,
   },
 })
